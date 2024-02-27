66c032301f04df01040f9918ac3a9f821fb64766
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2024-02-27 12:49:40 -0700

    Renamed and fixed cards

--- Assets/Scripts/War/WarLogic.cs
+++ Assets/Scripts/War/WarLogic.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WarLogic : MonoBehaviour
 {
     // Will need a List for the images
+    [SerializeField] private List<Image> cardsImages = new List<Image>();
 }
