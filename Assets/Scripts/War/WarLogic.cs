1ca0bacb7168d37b731e226daef99d9779484514
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2024-02-27 10:38:47 -0700

    Changing cards name

--- Assets/Scripts/War/WarLogic.cs
+++ Assets/Scripts/War/WarLogic.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class WarLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Will need a List for the images
 }
