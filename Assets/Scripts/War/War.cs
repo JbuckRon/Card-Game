b598433c13b112caa9724130a746a645a49f4999
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2024-03-03 23:55:01 -0700

    Comparison for war
    You can now compare  the cards in war, needed logic is hand tracking and end game

--- Assets/Scripts/War/War.cs
+++ Assets/Scripts/War/War.cs
@@ -33,8 +33,9 @@ public class War : MonoBehaviour
 
     private int GetCardRank(Sprite cardSprite)
     {
+    
         //  GameManager.instance.cardDeck;
-        string[] cardName = cardSprite.name.Split(' ');
+        string[] cardName = cardSprite.name.Split('_');
         string rankString = cardName[0]; // Getting the rank based on Val's naming convetion 4BCU = 4 black of clubs JBH = Jack black of Hearts
 
 
@@ -72,13 +73,13 @@ public class War : MonoBehaviour
         {
             // TODO Add one where it keeps track of the players hand and who has what
             Debug.Log("Player 2 is higher");
-            txt_Winner.SetText("Player 2 is higher");
+            txt_Winner.SetText("Player 1 is higher");
         }
         else if (player1Rank < player2Rank)
         {
             // TEST
             Debug.Log("Player 1 is higher");
-            txt_Winner.SetText("Player 1 is higher");
+            txt_Winner.SetText("Player 2 is higher");
 
         }
         else if (player1Rank == player2Rank)
@@ -91,7 +92,7 @@ public class War : MonoBehaviour
 
     public void SwitchTurn()
     {
-        if (playerTurn == PlayerTurn.PLAYER1) { playerTurn = PlayerTurn.PLAYER2; player1Stack.enabled = false; player1Stack.enabled = true; }
+        if (playerTurn == PlayerTurn.PLAYER1) { playerTurn = PlayerTurn.PLAYER2; player1Stack.enabled = false; player2Stack.enabled = true; }
         else if(playerTurn == PlayerTurn.PLAYER2) { playerTurn = PlayerTurn.PLAYER1; player2Stack.enabled = false; player1Stack.enabled = true; }
     }
 }
